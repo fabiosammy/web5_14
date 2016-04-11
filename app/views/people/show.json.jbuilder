4041e87343a9dc4d70c189c66e3bcfab4db0dc79
Fabio L. Janiszevski <contato@fabiosammy.com>
2016-04-11 19:06:47 -0300

    Added a people scaffold

---
+++ app/views/people/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @person, :id, :name, :weight, :born, :description, :created_at, :updated_at
---
+++ db/migrate/20160411220609_create_people.rb
@@ -0,0 +1,12 @@
+class CreatePeople < ActiveRecord::Migration
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.integer :weight
+      t.date :born
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
